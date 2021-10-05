396f8f7d96c9dfe06e6cfb945650795e57b662c1
mrflashstudio <mrflashstudio@gmail.com>
2021-10-05 11:46:11 +0300

    update secrets

--- backend/Discord/discordHandler.php
+++ backend/Discord/discordHandler.php
@@ -1,8 +1,8 @@
 <?php
-	define('TOKEN', 'ODYzNzQ0MzAwMDE4MTcxOTE1.YOrWkA.W0m7gllc87vadJihLtEPjMJFg84');
-	define('OAUTH2_CLIENT_ID', '863744300018171915');
-	define('OAUTH2_CLIENT_SECRET', 'uZc0F0J1Lw46LXE832LPfLjPFa34myLf');
-	define('AUTHORIZE_URL', 'https://discord.com/api/oauth2/authorize?client_id=863744300018171915&redirect_uri=https%3A%2F%2Fmaple.software%2Fdashboard%2Fsettings&response_type=code&scope=identify');
+	define('TOKEN', 'ODk0ODUxODI2ODM3NTE2Mjk4.YVwBtQ.hgkSaEoKUqa_v4KEg9RGlo3MKbQ');
+	define('OAUTH2_CLIENT_ID', '894851826837516298');
+	define('OAUTH2_CLIENT_SECRET', 'FfBcjj9s6z8rQOtRgtpwSVWpDh4uNBx9');
+	define('AUTHORIZE_URL', 'https://discord.com/api/oauth2/authorize?client_id=894851826837516298&redirect_uri=https%3A%2F%2Fmaple.software%2Fdashboard%2Fsettings&response_type=code&scope=identify');
 	define('TOKEN_URL', 'https://discordapp.com/api/oauth2/token');
 	define('USER_INFO_URL', 'https://discordapp.com/api/users/');
 
